9b329d5e03d5f8f532630e2ccb944b35472f58a7
Nicolas Fontbonne <fontbonnen@gmail.com>
2018-12-29 20:39:30 +0100

    Final Screen

--- app/src/main/java/com/fontbonne/ley/clerc/lockbreaker/UserProfile.java
+++ app/src/main/java/com/fontbonne/ley/clerc/lockbreaker/UserProfile.java
@@ -7,6 +7,7 @@ public class UserProfile implements Serializable {
     String email;
     String username;
     String password;
+    int level;
     int easyScore;
     int normalScore;
     int hardScore;
@@ -14,11 +15,11 @@ public class UserProfile implements Serializable {
     public static final String USER_PROFILE_TAG = "USER_PROFILE";
 
 
-
     UserProfile(String email, String username, String password) {
         this.email = email;
         this.username = username;
         this.password = password;
+        this.level = 1;
         this.easyScore = 0;
         this.normalScore = 0;
         this.hardScore = 0;
